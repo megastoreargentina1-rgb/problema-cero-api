c3e52102446cd0154d5f86862077a6c2f39daaed
megastoreargentina1-rgb <megastoreargentina1@gmail.com>
2026-06-08 22:49:02 -0300

    Update reglasMED.js

--- config/reglasMED.js
+++ config/reglasMED.js
@@ -1,1 +1,47 @@
+/**
+ * ==========================================================
+ * PROBLEMA CERO
+ * MED v1.0
+ * Matriz de Evidencia Diagnóstica
+ * ==========================================================
+ */
 
+module.exports = {
+
+  puntuacion: {
+    evidencia0: 0,
+    evidencia1: 5,
+    evidencia2: 10,
+    evidencia3: 15,
+    confirmado: 20
+  },
+
+  bloqueos: {
+
+    OFERTA: {
+      minimoConfirmacion: 2
+    },
+
+    POSICIONAMIENTO: {
+      minimoConfirmacion: 2
+    },
+
+    ADQUISICION: {
+      minimoConfirmacion: 2
+    },
+
+    CONVERSION: {
+      minimoConfirmacion: 2
+    },
+
+    OPERACION: {
+      minimoConfirmacion: 2
+    },
+
+    FINANCIERO: {
+      minimoConfirmacion: 2
+    }
+
+  }
+
+};
