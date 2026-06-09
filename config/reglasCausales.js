d5b93dfe7a606ea590049fee48a6c7da3ba1b8c9
megastoreargentina1-rgb <megastoreargentina1@gmail.com>
2026-06-08 22:53:18 -0300

    Update reglasCausales.js

--- config/reglasCausales.js
+++ config/reglasCausales.js
@@ -1,1 +1,34 @@
+/**
+ * ==========================================================
+ * PROBLEMA CERO
+ * Reglas Causales v1.0
+ * ==========================================================
+ */
 
+module.exports = {
+
+  jerarquia: [
+    "OFERTA",
+    "POSICIONAMIENTO",
+    "ADQUISICION",
+    "CONVERSION",
+    "OPERACION",
+    "FINANCIERO"
+  ],
+
+  estados: {
+    CONFIRMADA: "CAUSA_RAIZ_CONFIRMADA",
+    PROBABLE: "CAUSA_RAIZ_PROBABLE",
+    NO_AUTORIZADA: "CAUSA_RAIZ_NO_AUTORIZADA"
+  },
+
+  precedencia: {
+    OFERTA: 1,
+    POSICIONAMIENTO: 2,
+    ADQUISICION: 3,
+    CONVERSION: 4,
+    OPERACION: 5,
+    FINANCIERO: 6
+  }
+
+};
