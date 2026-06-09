77436b5478a20bdaf1915c0c06ef9321395955e1
megastoreargentina1-rgb <megastoreargentina1@gmail.com>
2026-06-08 22:57:24 -0300

    Update motorCausal.js

--- motores/motorCausal.js
+++ motores/motorCausal.js
@@ -1,1 +1,103 @@
+/**
+ * ==========================================================
+ * PROBLEMA CERO
+ * Motor Causal v1.0
+ * ==========================================================
+ */
 
+const REGLAS =
+  require("../config/reglasCausales");
+
+function obtenerEstado(icd) {
+
+  if (icd >= 85) {
+    return REGLAS.estados.CONFIRMADA;
+  }
+
+  if (icd >= 70) {
+    return REGLAS.estados.PROBABLE;
+  }
+
+  return REGLAS.estados.NO_AUTORIZADA;
+}
+
+function existeBloqueoSuperior(
+  bloqueo,
+  confirmados
+) {
+
+  const prioridadActual =
+    REGLAS.precedencia[bloqueo];
+
+  return confirmados.some(
+    item =>
+      REGLAS.precedencia[item] <
+      prioridadActual
+  );
+}
+
+function motorCausal({
+  bloqueosConfirmados = [],
+  icd = 0,
+  existeEquivalencia = false
+} = {}) {
+
+  const descartados = [];
+
+  const candidatos =
+    bloqueosConfirmados.filter(
+      bloqueo => {
+
+        const descartar =
+          existeBloqueoSuperior(
+            bloqueo,
+            bloqueosConfirmados
+          );
+
+        if (descartar) {
+          descartados.push(
+            bloqueo
+          );
+        }
+
+        return !descartar;
+      }
+    );
+
+  let causaRaiz = null;
+
+  if (
+    candidatos.length > 0
+  ) {
+
+    causaRaiz =
+      candidatos.sort(
+        (a, b) =>
+          REGLAS.precedencia[a] -
+          REGLAS.precedencia[b]
+      )[0];
+
+  }
+
+  return {
+
+    causaRaiz,
+
+    estado:
+      obtenerEstado(icd),
+
+    bloqueosConfirmados,
+
+    bloqueosDescartados:
+      descartados,
+
+    autorizada:
+      icd >= 85 &&
+      !existeEquivalencia &&
+      !!causaRaiz
+
+  };
+}
+
+module.exports =
+  motorCausal;
